44cc29e33f49625a6b5d30e1ec001b2f5d5baaab
igoramarall2 <contato.conspiraigor@gmail.com>
2023-01-03 00:02:30 -0300

    Implementacao de abas na home

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,16 @@
 import flet as ft
-from flet import AppBar, ElevatedButton, Page, Text, View, colors
+from flet import (
+    AppBar,
+    ElevatedButton,
+    Page,
+    Text,
+    View,
+    colors,
+    Column,
+    Row,
+    Card,
+    Icon,
+)
 import pyrebase
 
 firebaseConfig = {
@@ -17,11 +28,96 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
 
+nome = ft.TextField(
+    label="Nome",
+    # hint_text="Seu email para login",
+    border=ft.InputBorder.NONE,
+    filled=True,
+    # width=300,
+)
+sobrenome = ft.TextField(
+    label="Sobrenome",
+    border=ft.InputBorder.NONE,
+    filled=True,
+    # width=300,
+)
+idade = ft.TextField(
+    label="Idade",
+    border=ft.InputBorder.NONE,
+    filled=True,
+    # width=300,
+)
+email = ft.TextField(
+    label="Email",
+    border=ft.InputBorder.NONE,
+    filled=True,
+    # width=300,
+)
+celular = ft.TextField(
+    label="Celular",
+    border=ft.InputBorder.NONE,
+    filled=True,
+    # width=300,
+)
+data = {
+    "nome": nome.value,
+    "sobrenome": sobrenome.value,
+    "idade": idade.value,
+    "email": email.value,
+    "celular": celular.value,
+}
+db.push(data)
+
+t = ft.Tabs(
+    selected_index=0,
+    animation_duration=300,
+    tabs=[
+        ft.Tab(
+            text="Cadastro",
+            icon=ft.icons.ACCOUNT_BOX,
+            content=ft.Card(
+                content=ft.Container(
+                    content=ft.Column(
+                        [
+                            ft.ListTile(
+                                leading=ft.Icon(ft.icons.ALBUM),
+                                title=ft.Text("The Enchanted Nightingale"),
+                                subtitle=ft.Text(
+                                    "Music by Julie Gable. Lyrics by Sidney Stein."
+                                ),
+                            ),
+                            ft.Row(
+                                [ft.TextButton("Buy tickets"), ft.TextButton("Listen")],
+                                alignment=ft.MainAxisAlignment.END,
+                            ),
+                            nome,
+                            sobrenome,
+                            idade,
+                            email,
+                            celular,
+                        ]
+                    ),
+                    width=400,
+                    padding=10,
+                )
+            ),
+        ),
+        ft.Tab(
+            tab_content=ft.Icon(ft.icons.SEARCH),
+            content=ft.Text("This is Tab 2"),
+        ),
+        ft.Tab(
+            text="Tab 3",
+            icon=ft.icons.SETTINGS,
+            content=ft.Text("This is Tab 3"),
+        ),
+    ],
+    expand=1,
+)
+
 
 def __view__():
     return View(
         "/home",
-        [
-            ft.Text("Home", size=50, weight=ft.FontWeight.BOLD),
-        ],
+        [ft.Text("Home", size=50, weight=ft.FontWeight.BOLD), t],
     )
